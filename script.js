4f12596be15af9c7e83af00f8cf9edb9fa5ef771
KseniaShamina <126622222+KseniaShamina@users.noreply.github.com>
2023-07-24 16:46:18 -0400

    hw3js

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
-let age = Number(prompt('Сколько вам лет'));
-let up = 17;
-let result = age + up;
-console.log(`Через ${up} вам будет ${result}`);
+let password = "пароль";
+prompt ("Введите пароль");
+if (password === 'пароль') {
+    console.log ("Пароль введен верно");
+} else {
+    console.log ("Пароль введен неправильно");
+}
+
+let c = 7;
+prompt ("Введите число");
+if (c > 0 && с < 10 ) {
+    console.log ("Верно");
+} else {
+    console.log ("Неверно");
+}
+
+let d = 150;
+let e = 8;
+if (d > 100 || e > 100) {
+    console.log ("Верно");
+} else {
+    console.log ("Неверно");
+}
+
+let a = '2';
+let b = '3';
+console.log(a + b);
+
+let month = Number(prompt("Введите номер месяца"));
+
+switch (month) {
+	case '1':
+		console.log('сезон: зима');
+		break;
+	case '2':
+		console.log('сезон: зима');
+		break;
+    case '3':
+		console.log('сезон: весна');
+		break;
+    case '4':
+		console.log('сезон: весна');
+		break;
+    case '5':
+		console.log('сезон: весна');
+		break;
+    case '6':
+		console.log('сезон: лето');
+		break;
+    case '7':
+		console.log('сезон: лето');
+		break;
+    case '8':
+		console.log('сезон: лето');
+		break;
+    case '9':
+		console.log('сезон: осень');
+		break;
+    case '10':
+		console.log('сезон: осень');
+		break;
+    case '11':
+		console.log('сезон: осень');
+		break;
+    case '12':
+		console.log('сезон: зима');
+		break;
+	default: console.log('Что-то тебя совсем далеко унесло');
+		break;
+}
